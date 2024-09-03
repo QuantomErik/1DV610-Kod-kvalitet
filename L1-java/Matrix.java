216527f594eca9fae663467862cb9f1ec20fd1fd
QuantomErik <ey222ci@student.lnu.se>
2024-09-03 16:09:03 +0200

    Matrix command line application version 1.0

--- L1-java/Matrix.java
+++ L1-java/Matrix.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Matrix {
 
-    
+   private String name;
+   private Scanner scanner;
+
+   // ANSI escape codes for colors
+   public static final String ANSI_RED = "\u001B[31m";
+   public static final String ANSI_GREEN = "\u001B[32m";
+   public static final String ANSI_RESET = "\u001B[0m";
+
+   //Constructor
+   public Matrix() {
+    this.scanner = new Scanner(System.in);
+    this.name = "";
+   }
+
+   public void start() throws InterruptedException{
+    System.out.println(ANSI_GREEN + "Operator:...Who is this? ");
+   }
+
+
+    public static void main(String[] args) {
+        try {
+            Matrix matrixGame = new Matrix();
+            matrixGame.start();
+        } catch (InterruptedException e) {
+            System.out.println("This line is not secure...");
+        }
+    }
+
+
 
 }
