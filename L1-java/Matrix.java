d2b3373aa27f9d1e9bd1d4ded44137972e353d01
QuantomErik <ey222ci@student.lnu.se>
2024-09-03 16:36:54 +0200

    Create first multi choise question.

--- L1-java/Matrix.java
+++ L1-java/Matrix.java
@@ -20,6 +20,23 @@ public class Matrix {
     System.out.println(ANSI_GREEN + "Operator:...Who is this? ");
     this.name = scanner.nextLine();
 
+    simulateTypingEffect("Morpheus: Hello " + this.name + "...I've been looking for you. I image that right now you're feeling a bit like Alice. Tumbling down the rabbit hole?");
+
+    simulateTypingEffect("Morpheus: Do you belive in fate, " + this.name + "?");
+    System.out.println("1: Yes");
+    System.out.println("2: No");
+
+    String response = scanner.nextLine();
+
+    if (response.equals("1") || response.equals("2")) {
+        simulateTypingEffect("Morpheus: I know exactly what you mean. Let me tell you why you're here. You're here because you know something. What you know, you can't explain. But you feel it. You've felt it your entire life. Do you know what I'm talking about?");
+    } else {
+        simulateTypingEffect("Morpheus: Interesting choice, but the truth remains elusive.");
+        return;
+    }
+
+
+
 
    }
 
